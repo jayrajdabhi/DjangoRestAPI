e4c6620abb7e866a3626bd76bab4ae45dedcb6d7
Jayraj Dabhi <dabhi.jayraj78#gmail.com>
2024-11-03 16:48:59 -0500

    login and signup endpoints added

--- todobackend/api/views.py
+++ todobackend/api/views.py
@@ -1,8 +1,15 @@
 from rest_framework import generics,permissions
-from .serializers import TodoSerializer
+from .serializers import TodoSerializer, TodoToggleCompleteSerializer
 from todo.models import Todo
+from django.db import IntegrityError
+from django.contrib.auth.models import User
+from rest_framework.parsers import JSONParser
+from rest_framework.authtoken.models import Token
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate
 
-
+ 
 class TodoListCreate(generics.ListCreateAPIView):
     serializer_class = TodoSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -19,3 +26,61 @@ class TodoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     def get_queryset(self):
         user = self.request.user
         return Todo.objects.filter(user=user)
+
+class TodoToggleComplete(generics.UpdateAPIView):
+    serializer_class = TodoToggleCompleteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user)
+    def perform_update(self,serializer):
+        serializer.instance.completed=not(serializer.instance.completed)
+        serializer.save()
+
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        try:
+            data = JSONParser().parse(request)  # data is a dictionary
+            user = User.objects.create_user(
+                username=data['username'],
+                password=data['password']
+            )
+            user.save()
+            token = Token.objects.create(user=user)
+            return JsonResponse({'token': str(token)}, status=201)
+
+        except IntegrityError:
+            return JsonResponse(
+                {'error': 'username taken. choose another username'},
+                status=400
+            )
+    else:
+        return JsonResponse({'error': 'Invalid request method.'}, status=405)
+
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        try:
+            data = JSONParser().parse(request)
+        except ParseError:
+            return JsonResponse({'error': 'Invalid JSON'}, status=400)
+
+        user = authenticate(
+            request,
+            username=data.get('username'),
+            password=data.get('password')
+        )
+
+        if user is None:
+            return JsonResponse(
+                {'error': 'unable to login. check username and password'},
+                status=400
+            )
+        else:
+            try:
+                token = Token.objects.get(user=user)
+            except Token.DoesNotExist:
+                token = Token.objects.create(user=user)
+
+            return JsonResponse({'token': str(token)}, status=201)
--- todobackend/backend/settings.py
+++ todobackend/backend/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'todo',
     'rest_framework',
-    'api'
+    'api',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+        'DEFAULT_AUTHENTICATION_CLASSES':
+        [
+        'rest_framework.authentication.TokenAuthentication',
+            ]
+}
